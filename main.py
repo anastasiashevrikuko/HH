d46a54fdc243c24c9fd4ea4545ae34945b687d44
anastasiashevrikuko <shevrikuko.nastia@gmail.com>
2025-07-19 17:33:38 +0500

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import json
 import os
 
 HH_URL = 'https://hh.ru/vacancies/product_manager'
-HEADERS = {'User-Agent': 'Mozilla/5.0'}
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+}
 SEEN_FILE = 'seen.json'
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
